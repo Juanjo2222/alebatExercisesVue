cb8e1ffbe928e26aac7a12f716ddd7e0b51140a2
Juanjo <juanjosemillanguerra@gmail.com>
2025-03-30 19:14:14 +0200

    exercise 9 completed

--- vueProject/src/components/ChildComponent.vue
+++ vueProject/src/components/ChildComponent.vue
@@ -8,12 +8,18 @@ const emit = defineEmits<{
 const sendMessage = () => {
   emit('message-sent', 'Hola padre')
 }
+
+defineProps<{
+    parentMessage: string
+}>()
+
 </script>
 
 <template>
     <section class="child-component">
         <h1>Este es el hijo</h1>
         <button class="send-button" @click="sendMessage">Enviar al padre</button>
+        <h2>{{ parentMessage }}</h2>
     </section>
 </template>
 
--- vueProject/src/components/ParentComponent.vue
+++ vueProject/src/components/ParentComponent.vue
@@ -7,12 +7,15 @@ const childsMessage = ref('');
 const showMessage = (message: string) => {
     childsMessage.value = message;
 }
+
+const parentMessage = "Hola hijo";
+
 </script>
 
 <template>
     <section class="parent-component">
         <h1>Este es el padre</h1>
-        <ChildComponent @message-sent="showMessage" />
+        <ChildComponent :parentMessage="parentMessage" @message-sent="showMessage"/>
         <h1>{{ childsMessage }}</h1>
     </section>
 </template>
