b93ada4871d7c29503af72252c73765433d04bba
Juan Jose <juanjomillanguerra@gmail.com>
2025-03-31 12:38:34 +0200

    exercise 12 completed

--- vueProject/src/components/TheCounter.vue
+++ vueProject/src/components/TheCounter.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import TheTitle from './TheTitle.vue'
-import { increment, decrement, counter } from "@/composables/TheCounter.vue";
+import { increment, decrement, counter } from '@/composables/TheCounter.vue'
 </script>
 
 <template>
   <section>
-    <TheTitle #title>Título estático</TheTitle>
-    <p>Contador = {{ counter }}</p>
+    <TheTitle>
+      <template #title>Título estático</template>
+    </TheTitle>
+    <p :class="counter === 10 ? 'active' : 'inactive'">Contador = {{ counter }}</p>
     <button class="increment" @click="increment" v-if="counter <= 10">Incrementar</button>
     <button class="decrement" @click="decrement" v-if="counter !== 0">Decrementar</button>
   </section>
 </template>
+
+<style>
+.active {
+  color: green;
+}
+</style>
