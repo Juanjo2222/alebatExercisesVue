2c2a9645bcd2e96c9a48248c85d85310a4a23ed1
Juan Jose <juanjomillanguerra@gmail.com>
2025-04-01 08:50:03 +0200

    exercise 9 correction

--- vueProject/src/components/ChildComponent.vue
+++ vueProject/src/components/ChildComponent.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import ParentComponent from '@/components/ParentComponent.vue';
-
 const emit = defineEmits<{
   (event: 'message-sent', message: string): void
 }>()
@@ -10,22 +8,21 @@ const sendMessage = () => {
 }
 
 defineProps<{
-    parentMessage: string
+  parentMessage: string
 }>()
-
 </script>
 
 <template>
-    <section class="child-component">
-        <h1>Este es el hijo</h1>
-        <button class="send-button" @click="sendMessage">Enviar al padre</button>
-        <h2>{{ parentMessage }}</h2>
-    </section>
+  <section class="child-component">
+    <h1>Este es el hijo</h1>
+    <button class="send-button" @click="sendMessage">Enviar al padre</button>
+    <h2>{{ parentMessage }}</h2>
+  </section>
 </template>
 
 <style>
-    .child-component{
-        background-color: rgb(168, 214, 214);
-        padding:2rem;
-    }
+.child-component {
+  background-color: rgb(168, 214, 214);
+  padding: 2rem;
+}
 </style>
